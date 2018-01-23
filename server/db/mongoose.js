9f9708e31390386d74fd330b95f6fa205b9cff4e
codeblood <ankitshr8@gmail.com>
2018-01-23 09:54:00 +0530

    local url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ var mongoose =  require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://root:root@ds129144.mlab.com:29144/apidatabase"  );
+mongoose.connect( "mongodb://localhost:27017/test" ||"mongodb://root:root@ds129144.mlab.com:29144/apidatabase"  );
 module.exports = {mongoose};
